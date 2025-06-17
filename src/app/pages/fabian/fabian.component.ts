286e65653515fd8f81b3f5b5c0ce5f73b36ec129
Fabián Chacón <115729645+fchaconb@users.noreply.github.com>
2025-06-16 21:09:58 -0600

    boton fabian

---
+++ src/app/pages/fabian/fabian.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fabian',
+  standalone: true,
+  imports: [],
+  templateUrl: './fabian.component.html',
+  styleUrl: './fabian.component.scss'
+})
+export class FabianComponent {
+
+}
