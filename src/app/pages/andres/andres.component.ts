a9597de6009f08fc72e433f7b5382dcb0262634b
AndresSC <aseguracalderon26@gmail.com>
2025-06-16 21:04:36 -0600

    Commit Andres
    ASC commit

---
+++ src/app/pages/andres/andres.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-andres',
+  standalone: true,
+  imports: [],
+  templateUrl: './andres.component.html',
+  styleUrl: './andres.component.scss'
+})
+export class AndresComponent {
+
+}
