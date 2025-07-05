bf8e2677168b256a69fac89c7690a04d98ba7051
AndresSC <aseguracalderon26@gmail.com>
2025-07-05 10:23:41 -0600

    Edit and adding users disabled and deleting user confirmartion.

--- src/app/components/user/user-list/user-list.component.ts
+++ src/app/components/user/user-list/user-list.component.ts
@@ -1,25 +1,37 @@
-import { Component, effect, EventEmitter, inject, Input, Output } from '@angular/core';
-import { UserService } from '../../../services/user.service';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IUser } from '../../../interfaces';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ModalComponent } from '../../modal/modal.component';
-import { UserFormComponent } from '../user-from/user-form.component';
-import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 
+declare var bootstrap: any; // permite usar Bootstrap modal
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
-  @Input() title: string  = '';
+  @Input() title: string = '';
   @Input() users: IUser[] = [];
-  @Output() callModalAction: EventEmitter<IUser> = new EventEmitter<IUser>();
-  @Output() callDeleteAction: EventEmitter<IUser> = new EventEmitter<IUser>();
+  @Output() callModalAction = new EventEmitter<IUser>();
+  @Output() callDeleteAction = new EventEmitter<IUser>();
+
+  usuarioAEliminar: IUser | null = null;
+
+  abrirModalConfirmacion(usuario: IUser): void {
+    this.usuarioAEliminar = usuario;
+    const modal = new bootstrap.Modal(document.getElementById('modalConfirmacion'));
+    modal.show();
+  }
+
+  confirmarEliminacion(): void {
+    if (this.usuarioAEliminar) {
+      this.callDeleteAction.emit(this.usuarioAEliminar);
+    }
+
+    const modalElement = document.getElementById('modalConfirmacion');
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
+  }
 }
