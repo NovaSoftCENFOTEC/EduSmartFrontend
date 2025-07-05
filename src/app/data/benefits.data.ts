42d23b4132ce6c8752cbafa99b1da4919ed7cdc1
Pablo Navarro <pnavarro0923@gmail.com>
2025-07-05 00:42:44 -0600

    feat: benefits components created

---
+++ src/app/data/benefits.data.ts
@@ -0,0 +1,17 @@
+export const BENEFITS = [
+    {
+        icon: 'bi-book',
+        title: 'Contenido interactivo',
+        text: 'Sumérgete en la Historia de Costa Rica con narraciones creativas que convierten cada capítulo en un cuento cautivador. Hace de cada lección una experiencia única, dinámica y fácil de entender.'
+    },
+    {
+        icon: 'bi-award',
+        title: 'Retos y recompensas',
+        text: 'Pon a prueba tus conocimientos con quizzes interactivos al finalizar cada lección. Gana medallas exclusivas como recompensa y desbloquea puntos extra explorando la sección de Explora+, donde descubrirás datos sorprendentes.'
+    },
+    {
+        icon: 'bi-robot',
+        title: 'Tu guía personalizada: TicoBot',
+        text: 'Conversa en tiempo real con TicoBot, nuestro chatbot inteligente. Haz preguntas, aclara dudas y recibe orientación personalizada para que tu experiencia de aprendizaje sea fluida, divertida y adaptada a tu ritmo.'
+    }
+];
--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TopbarComponent } from '../../components/app-layout/elements/topbar/topbar.component';
+import { BenefitsComponent } from '../../components/benefits/benefits.component';
+
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [TopbarComponent],
+  imports: [TopbarComponent, BenefitsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
