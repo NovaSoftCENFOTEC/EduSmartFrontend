e7c435da1aabbf5f96d5bf5321076d686fe4287e
Pablo Navarro <pnavarro0923@gmail.com>
2025-07-05 01:10:02 -0600

    feat: footer created

--- src/app/data/benefits.data.ts
+++ src/app/data/benefits.data.ts
@@ -2,7 +2,7 @@ export const BENEFITS = [
     {
         icon: 'bi-book',
         title: 'Contenido interactivo',
-        text: 'Sumérgete en la Historia de Costa Rica con narraciones creativas que convierten cada capítulo en un cuento cautivador. Hace de cada lección una experiencia única, dinámica y fácil de entender.'
+        text: 'Sumérgete en la historia de Costa Rica con narraciones creativas que convierten cada capítulo en un cuento cautivador. Realiza de cada lección una experiencia única, dinámica y fácil de entender.'
     },
     {
         icon: 'bi-award',
@@ -11,7 +11,7 @@ export const BENEFITS = [
     },
     {
         icon: 'bi-robot',
-        title: 'Tu guía personalizada: TicoBot',
+        title: 'Guía personalizada: TicoBot',
         text: 'Conversa en tiempo real con TicoBot, nuestro chatbot inteligente. Haz preguntas, aclara dudas y recibe orientación personalizada para que tu experiencia de aprendizaje sea fluida, divertida y adaptada a tu ritmo.'
     }
 ];
--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { TopbarComponent } from '../../components/app-layout/elements/topbar/topbar.component';
 import { BenefitsComponent } from '../../components/benefits/benefits.component';
+import { FooterComponent } from '../../components/app-layout/elements/footer/footer.component';
 
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [TopbarComponent, BenefitsComponent],
+  imports: [TopbarComponent, BenefitsComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
