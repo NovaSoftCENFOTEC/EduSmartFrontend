4eaa585632d65fb25bc5178d6a90ca85e016ff88
AndresSC <aseguracalderon26@gmail.com>
2025-07-28 19:17:24 -0600

    routes restriction added

--- src/app/components/app-layout/app-layout.component.ts
+++ src/app/components/app-layout/app-layout.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {TopbarComponent} from './elements/topbar/topbar.component';
-import {SidebarComponent} from './elements/sidebar/sidebar.component';
-import {CommonModule} from '@angular/common';
-import {LayoutService} from '../../services/layout.service';
-import {FooterComponent} from "./elements/footer/footer.component";
-import {NavbarComponent} from '../navbar/navbar.components';
+import { Component } from '@angular/core';
+import { Router, NavigationEnd, RouterLink, RouterOutlet } from '@angular/router';
+import { TopbarComponent } from './elements/topbar/topbar.component';
+import { SidebarComponent } from './elements/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { LayoutService } from '../../services/layout.service';
+import { FooterComponent } from "./elements/footer/footer.component";
+import { NavbarComponent } from '../navbar/navbar.components';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'app-layout',
@@ -23,9 +24,26 @@ import {NavbarComponent} from '../navbar/navbar.components';
 })
 export class AppLayoutComponent {
     public title?: string;
+    public shouldHideChat: boolean = false;
 
-    constructor(public layoutService: LayoutService) {
+    constructor(
+        public layoutService: LayoutService,
+        private router: Router
+    ) {
         this.layoutService.title.subscribe((title) => (this.title = title));
-    }
 
+        this.router.events.pipe(
+
+            filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+        ).subscribe((event) => {
+            const hiddenRoutes = [
+                //TODO: Add more routes to hide chat
+                '/app/chat',
+            ];
+
+            this.shouldHideChat = hiddenRoutes.some(route =>
+                event.urlAfterRedirects.startsWith(route)
+            );
+        });
+    }
 }
