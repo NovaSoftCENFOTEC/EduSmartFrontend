f64600fc24e3d946d14cc8d680b7cb59f4f71577
AndresSC <aseguracalderon26@gmail.com>
2025-07-29 19:54:16 -0600

    minor fixes

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    standalone: true,
+    styleUrls: ['./product.component.scss']
 })
-export class ProductComponent { 
-  goToLanding() {
-  window.location.href = '/landing';
-}
-goToHome() {
-  window.location.href = '/';
-}
+export class ProductComponent {
+    goToLanding() {
+        window.location.href = '/landing';
+    }
+
+    goToHome() {
+        window.location.href = '/';
+    }
 }
