f995acd5deade28aea7e145f835467b0bb4d2536
GiaMQ <gmonteroq@ucenfotec.ac.cr>
2025-08-01 01:32:52 -0400

    Fixed conflicts

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,12 +1,12 @@
-import {Component} from '@angular/core';
-
+import { Component } from '@angular/core';
 import { CustomTopbarComponent } from '../../components/app-layout/elements/custom-topbar/custom-topbar.component';
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html',
     standalone: true,
-    styleUrls: ['./product.component.scss']
+    styleUrls: ['./product.component.scss'],
+    imports: [CustomTopbarComponent]
 })
 export class ProductComponent {
     goToLanding() {
