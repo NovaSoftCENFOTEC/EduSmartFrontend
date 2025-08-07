efab0f5ad2917c6f3b1c4a93d06f57fa18239021
AndresSC <aseguracalderon26@gmail.com>
2025-08-06 21:55:56 -0600

    Minor fixes

--- src/app/pages/medal/medal.component.ts
+++ src/app/pages/medal/medal.component.ts
@@ -1,71 +1,68 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BadgeService } from '../../services/badge.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { IBadge } from '../../interfaces';
-import { PaginationComponent } from '../../components/pagination/pagination.component';
-import { AuthService } from '../../services/auth.service'; 
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BadgeService} from '../../services/badge.service';
+import {Router} from '@angular/router';
+import {IBadge} from '../../interfaces';
+import {PaginationComponent} from '../../components/pagination/pagination.component';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
-  selector: 'app-medal',
-  standalone: true,
-  imports: [CommonModule, PaginationComponent],
-  templateUrl: './medal.component.html',
-  styleUrls: ['./medal.component.scss']
+    selector: 'app-medal',
+    standalone: true,
+    imports: [CommonModule, PaginationComponent],
+    templateUrl: './medal.component.html',
+    styleUrls: ['./medal.component.scss']
 })
 export class MedalComponent implements OnInit {
-  public studentBadgeIds: number[] = [];
-  public allBadges: IBadge[] = [];
-  public showModal: boolean = false;
-  public selectedBadge?: IBadge;
+    public studentBadgeIds: number[] = [];
+    public allBadges: IBadge[] = [];
+    public showModal: boolean = false;
+    public selectedBadge?: IBadge;
 
-  constructor(
-    public badgeService: BadgeService,
-    private router: Router,
-    private authService: AuthService 
-  ) {}
+    constructor(
+        public badgeService: BadgeService,
+        private router: Router,
+        private authService: AuthService
+    ) {
+    }
 
-  ngOnInit(): void {
-    const user = this.authService.getUser();
-    const studentId = user?.id;
+    get medals() {
+        return this.allBadges;
+    }
 
-    this.badgeService.getAll();
-    setTimeout(() => {
-      this.allBadges = this.badgeService.badges$();
-      
+    ngOnInit(): void {
+        const user = this.authService.getUser();
+        const studentId = user?.id;
 
-      if (typeof studentId === 'number') {
-        this.badgeService.getBadgesByStudent(studentId);
+        this.badgeService.getAll();
         setTimeout(() => {
-          const studentBadges = this.badgeService.badges$();
-          this.studentBadgeIds = studentBadges.map(badge => badge.id!).filter(id => id !== undefined);
+            this.allBadges = this.badgeService.badges$();
+
+            if (typeof studentId === 'number') {
+                this.badgeService.getBadgesByStudent(studentId);
+                setTimeout(() => {
+                    const studentBadges = this.badgeService.badges$();
+                    this.studentBadgeIds = studentBadges.map(badge => badge.id!).filter(id => id !== undefined);
+                }, 500);
+            }
         }, 500);
-      } else {
-        console.warn('No se encontró el studentId del usuario logueado.');
-      }
-    }, 500);
-  }
+    }
 
-  get medals() {
-    return this.allBadges;
-  }
+    isUnlocked(medalId: number): boolean {
+        return this.studentBadgeIds.includes(medalId);
+    }
 
-  isUnlocked(medalId: number): boolean {
-    return this.studentBadgeIds.includes(medalId);
-  }
+    goBack(): void {
+        this.router.navigate(['/app/student-groups']);
+    }
 
-  goBack(): void {
-    this.router.navigate(['/app/student-groups']);
-  }
+    openModal(badge: IBadge): void {
+        this.selectedBadge = badge;
+        this.showModal = true;
+    }
 
-  openModal(badge: IBadge): void {
-     console.log('Datos del badge seleccionado:', badge);
-    this.selectedBadge = badge;
-    this.showModal = true;
-  }
-
-  closeModal(): void {
-    this.showModal = false;
-    this.selectedBadge = undefined;
-  }
+    closeModal(): void {
+        this.showModal = false;
+        this.selectedBadge = undefined;
+    }
 }
